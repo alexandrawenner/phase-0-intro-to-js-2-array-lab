56b13fab89c57418d48a2c071643a12d4e047e81
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-05-20 08:28:30 -0400

    commit tree 5ea8624ed3f55876e4e05987750ab1b1dfacacdd

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+
+const cats = ['Milo','Otis','Garfield']
+
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const appCats = [...cats, name];
+    return appCats
+}
+
+function prependCat(name) {
+    const prepCats = [name, ...cats];
+    return prepCats
+}
+
+function removeLastCat() {
+    const byeLastCat = cats.slice(0,2)
+    return byeLastCat
+}
+
+function removeFirstCat() {
+    const byeFirstCat = cats.slice(1)
+    return byeFirstCat
+}
+
